78262e3ce6a735f018ce7766184dabc7f5cddf24
akash-912 <akashdeep12896@gmail.com>
2026-07-06 01:15:01 +0530

    auth

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,17 @@
-import { UserButton, SignInButton, Show } from '@clerk/nextjs'
+"use client"
+
+import { UserButton, SignInButton} from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { Authenticated, Unauthenticated } from 'convex/react'
+import {BarLoader} from "react-spinners"
+import { useStoreUser } from '@/hooks/use-store-user'
 
 const Header = () => {
+
+  const {isLoading} = useStoreUser();
   return (
     <>
         <nav className='fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-xl z-20 border-b'>
@@ -18,24 +25,31 @@ const Header = () => {
                 {/* Right Side Actions  */}
                 <div className='flex items-center'>
                   <div className="flex items-center gap-4 p-2">
-                  <Show when="signed-in">
+                  <Authenticated>
                     {/* UserButton handles its own styling nicely, so we just wrap it for layout */}
                     <div className="ring-2 ring-slate-200 rounded-full hover:ring-slate-300 transition-all">
                       <UserButton />        
                     </div>
-                  </Show>
+                  </Authenticated>
 
-                  <Show when="signed-out">
+                  <Unauthenticated>
                     {/* By nesting a standard HTML button inside, Clerk applies the click logic to it while you control the Tailwind design */}
                     <SignInButton mode='modal'>
                       <Button size='sm'>Sign In</Button>
                     </SignInButton>
-                  </Show>
+                  </Unauthenticated>
                 </div>
                 </div>
             </div>
 
             {/* Mobile Search & Location - Below Header  */}
+
+            {/* Loader  */}{
+            isLoading&&
+            <div className='absolute bottom-0 left-0 w-full'>
+              <BarLoader width={'100%'} color='#a855f7'/>
+            </div>
+            }
         </nav>
 
         {/* Modals  */}
---
+++ convex/schema.js
@@ -0,0 +1,33 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    // users Table
+
+    users: defineTable({
+        name: v.string(),
+        tokenIdentifier: v.string(), // Clerk user ID for auth 
+        email: v.string(),
+        imageUrl: v.optional(v.string()),
+
+        // Onboarding
+        hasCompletedOnboarding: v.boolean(),
+
+        location: v.optional(
+            v.object({
+                city: v.string(),
+                state: v.optional(v.string()),
+                country: v.string(),
+            })
+        ),
+
+        interests: v.optional(v.array(v.string())),
+
+        freeEventsCreated: v.number(),
+        
+        createdAt: v.number(),
+        updatedAt: v.number(),
+
+
+    }).index("by_token",["tokenIdentifier"]),
+});
