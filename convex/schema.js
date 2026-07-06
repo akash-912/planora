9854e40433af9db36ba128555f5ca4848028fe06
akash-912 <akashdeep12896@gmail.com>
2026-07-07 00:52:35 +0530

    Landing page

--- convex/schema.js
+++ convex/schema.js
@@ -1,5 +1,6 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
+import { QrCode } from "lucide-react";
 
 export default defineSchema({
     // users Table
@@ -31,6 +32,75 @@ export default defineSchema({
 
     }).index("by_token",["tokenIdentifier"]),
 
+    events: defineTable({
+        title: v.string(),
+        description: v.string(),
+        slug: v.string(),
 
+        // Organizer
+        organizerId: v.id("users"),
+        organizerName: v.string(),
+
+        // Event details
+        category: v.string(),
+        tags: v.array(v.string()),
+
+        // Date & time
+        startDate: v.number(),
+        endDate: v.number(),
+        timeZone: v.string(),
+
+        // Location
+        locationType: v.union(v.literal("physical"), v.literal("online")),
+        venue: v.optional(v.string()),
+        address: v.optional(v.string()),
+        city: v.string(),
+        state: v.optional(v.string()),
+
+        // Capacity & Ticketing
+        capacity: v.number(),
+        ticketType: v.union(v.literal("free"),v.literal("paid")),
+        tickerPrice: v.optional(v.number()),
+        registrationCount: v.number(),
+
+        // Customization
+        coverImage: v.optional(v.string()),
+        themeColor: v.optional(v.string()),
+
+        // TimeStamps
+        createdAt: v.number(),
+        updatedAt: v.number(),
+
+    })
+        .index("by_organizer", ["organizerId"])
+        .index("by_category",["category"])
+        .index("by_start_date",["startDate"])
+        .index("by_slug",["slug"])
+        .searchIndex("search_title",{searchField: "title"}),
+
+    registrations: defineTable({
+        eventId: v.id("events"),
+        userId: v.id("users"),
+
+        // Attendee info
+        attendeeName: v.string(),
+        attendeeEmail: v.string(),
+
+        // QR Code for entry
+        qrCode: v.string(),
     
+        // Check-in
+        checkedIn: v.boolean(),
+        checkedInAt: v.optional(v.number()),
+
+        // Status
+        status: v.union(v.literal("confirmed"),v.literal("cancelled")),
+
+        registeredAt: v.number(),
+    })
+        .index("by_event",["eventId"])
+        .index("by_user",["userId"])
+        .index("by_event_user",["eventId","userId"])
+        .index("by_qr_code",["qrCode"]),
+
 });
